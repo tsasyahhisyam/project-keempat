846b98e7d210e615e24ce27d040eeba3748a24c5
Tsasyah Hisyam S.P <44688025+tsasyahhisyam@users.noreply.github.com>
2018-11-09 11:02:50 +0700

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+import { RecipeDetailComponent } from "../app/recipe/recipe-detail/recipe-detail.component";
+import { RecipeListComponent } from "../app/recipe/recipe-detail/recipe-list/recipe-list.component";
+import { RecipeItemComponent } from "../app/recipe/recipe-detail/recipe-list/recipe-item/recipe-item.component";
+import { ShoppingEditComponent } from "../app/shopping-list/shopping-edit/shopping-edit.component";
+import { DropdownDirective } from '../app/shared/dropdown.directive';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipeComponent,
+    ShoppingListComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent,
+    DropdownDirective
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [ShoppingListService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
